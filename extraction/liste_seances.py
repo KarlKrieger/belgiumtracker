e4abcd4c8d70583647d4cfd08f9cf632e781ba6f
Adeuxline <laberomain@gmail.com>
2018-12-30 21:43:43 +0200

    Update liste_seances.py

--- extraction/liste_seances.py
+++ extraction/liste_seances.py
@@ -10,46 +10,77 @@ import urllib.request
 
 # Nombre de séances plénières à afficher. S'il faut les afficher toutes, 
 # retirer l'attribut 'limit' dans la première boucle for
-NB_MAX = 25
+NB_MAX = 264
+
+N_LEGISLATURE = 54
 
 # Récupération du code source de la page
-html_doc = urllib.request.urlopen('http://www.lachambre.be/kvvcr/showpage.cfm?section=/cricra&language=fr&cfm=dcricra.cfm?type=plen&cricra=cri&count=all&legislat=54')
+
+url = 'http://www.lachambre.be/kvvcr/showpage.cfm?section=/cricra&language=fr&cfm=dcricra.cfm?type=plen&cricra=cri&count=all&legislat=' + 'N_LEGISTALURE'
+html_doc = urllib.request.urlopen(url)
 
 soup = BeautifulSoup(html_doc, 'html.parser')
 
+doc = open('seances.csv', 'w')
+doc.write('name,legislature_id,date,moment,approuve' + '\n')
 for link in soup.find_all('tr', valign='top', limit=NB_MAX):
-    
+    record = ''
     # Numéro de la séance
     for line in link.find_all('td', height="25px", limit=1):
         for data in line.find_all('a'):
-            print(str('ID:       ') + data.string)
-    
+            print(str('ID:        ') + data.string)
+            record = record + data.string + ','
+    # Numéro de la législature
+    print('Legistature: ' + str(N_LEGISLATURE))
+    record = record + str(N_LEGISLATURE) + ','
+    # Récupération de la date: JJ mois YYYY
+    for line in link.find_all('td', style='line-height: 1.0em;', width='110px', limit=1):
+        line = " ".join(line.string.split())
+        print(str('Date:      ') + line)
+        jour, mois, annee = line.split(' ',2)
+        def mois_en_chiffre(mois):
+            return {
+                'janvier': '01',
+                'février': '02',
+                'mars': '03',
+                'avril': '04',
+                'mai': '05',
+                'juin': '06',
+                'juillet': '07',
+                'août': '08',
+                'septembre': '09',
+                'octobre': '10',
+                'novembre': '11',
+                'décembre': '12'
+            }.get(mois, 'NOT FOUND')
+        record = record + annee + str(mois_en_chiffre(mois)) + jour + ','
+
     # Récupération du jour et du moment de la journée (AM, PM, Soir)
     for line in link.find_all('i', style='font-size: 1.0em;color: 999999;', limit=1):
         line = " ".join(line.string.split())
         jour, moment = line.split(' ',1)
-        print(str('Moment:   ') + str(moment) + '\n' + str('Jour:     ' + str(jour)))
+        print(str('Moment:    ') + str(moment) + '\n' + str('Jour:      ' + str(jour)))
+        record = record + moment + ','
 
-    # Récupération de la date: JJ mois YYYY
-    for line in link.find_all('td', style='line-height: 1.0em;', width='110px', limit=1):
-        print(str('Date:     ') + " ".join(line.string.split()))
-    
     # Récupération du lien pour le pdf structuré, la brochure bilingue et le contenu en html
     for line in link.find_all('a', title='La brochure imprim?e', limit=1):
-        print(str('Pdf:      ') + str('www.lachambre.be') + line.get('href'))
+        print(str('Pdf:       ') + str('www.lachambre.be') + line.get('href'))
     for line in link.find_all('a', title='PDF structuré', limit=1):
-        print(str('Pda:      ') + str('www.lachambre.be') + line.get('href'))
+        print(str('Pda:       ') + str('www.lachambre.be') + line.get('href'))
     for line in link.find_all('a', title='Version HTML prête à copier', limit=1):
-        print(str('Text:     ') + str('www.lachambre.be') + line.get('href'))
+        print(str('Text:      ') + str('www.lachambre.be') + line.get('href'))
     
     # Récupération du statut du PV de la séance (approuvé ou pas)
     for line in link.find_all('i', style='font-size: 0.9em;color: 999999;', limit=1):
         line = " ".join(line.string.split())
         if line == 'version définitive':
             version = 'True (définitive)'
+            aprouve = 'true'
         else:
             version = 'False (provisoire)'
-        print(str('Version:  ') + version)
+            approuve = 'false'
+        print(str('Version:   ') + version)
+        record = record + approuve
     
     # Vérifie s'il y a une annexe ou pas. Si oui, donne le lien. Sinon, n'affiche rien.
     # limit=4 parce le style est le même que pour les 3 documents 
@@ -58,4 +89,7 @@ for link in soup.find_all('tr', valign='top', limit=NB_MAX):
         for data in line.find_all('a'):
             if data.string == 'Annexe':
                 print('Annexe:   ' + 'www.lachambre.be' + data.get('href'))
+    print('csv:      ' + record)
+    doc.write(record + '\n')
     print('-------------------------------------')
+doc.close
